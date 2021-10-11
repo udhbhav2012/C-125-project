f8f54445bf9b4d21757371a6cca63933aff0647f
udhbhav2012 <82303525+udhbhav2012@users.noreply.github.com>
2021-10-11 17:06:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rigthWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Posenet is initialized");
+}
+
+function draw() {
+    background('#87CEEB');
+    document.getElementById("square_side").innerHTML = "Width and Height of a square will be = " + difference + "px";
+    fill('#f90093');
+    stroke('#f90093');
+    square(noseX, noseY, difference);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference);
+        
+
+    }
+}
+
